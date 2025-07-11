12e43748ab6b813a2714556d6f189a5d125750e1
Suraj Kumar <152789285+Suraj-Kumar83@users.noreply.github.com>
2025-07-11 23:00:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,50 @@
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const session = require("express-session");
 const passport = require("passport");
-const registerRoute =require("./Routes/Register");
-const ChatRoute = require("./Routes/Chat");
 const path = require("path");
-require("dotenv").config();
-require("./config/passport"); 
 const cookieParser = require("cookie-parser");
+require("dotenv").config();
+require("./config/passport");
+
+const registerRoute = require("./Routes/Register");
+const ChatRoute = require("./Routes/Chat");
 const authRoute = require("./Routes/AuthRoute");
 const proposalRoute = require("./Routes/Proposal.js");
 const govSignupRoute = require("./Routes/GovRoute.js");
 const govLoginRoute = require("./Routes/govAuth.js");
 
-
-const MONGO_URL = process.env.MONGO_URL;
-const PORT = process.env.PORT || 4000;
-
 const app = express();
-
-mongoose
-  .connect(MONGO_URL)
-  .then(() => console.log("MongoDB is connected successfully"))
-  .catch((err) => console.error(err));
+const PORT = process.env.PORT || 4000;
+const MONGO_URL = process.env.MONGO_URL;
 
 app.use(
   cors({
-    origin: [ "http://localhost:3000","https://ayush-startup-frontend.vercel.app/"],
-    methods: ["GET", "POST","PUT", "DELETE", "OPTIONS"],
+    origin: [
+      "http://localhost:3000", 
+      "https://ayush-startup-frontend.vercel.app", 
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
 
 app.options("*", cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-
 
 app.use(
   session({
-    secret: process.env.GOOGLE_CLIENT_SECRET, // replace with env var
+    secret: process.env.GOOGLE_CLIENT_SECRET,
     resave: false,
     saveUninitialized: false,
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -59,6 +54,12 @@ app.use("/api", ChatRoute);
 app.use("/api/proposal", proposalRoute);
 app.use("/signup/govsignup", govSignupRoute);
 app.use("/login/govlogin", govLoginRoute);
+
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("MongoDB is connected successfully"))
+  .catch((err) => console.error(err));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
