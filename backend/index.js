9d5d8d6b46e105cc9241bc1c43b095ea94e678f3
Suraj Kumar <152789285+Suraj-Kumar83@users.noreply.github.com>
2025-07-11 23:48:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,18 +19,18 @@ const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000", 
-      "https://ayush-startup-frontend.vercel.app", 
-    ],
-    methods: ["GET", "POST","PUT", "DELETE"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "https://ayush-startup-frontend.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
 
-app.options("*", cors());
+app.use(cors(corsOptions));           
+app.options("*", cors(corsOptions));  
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
